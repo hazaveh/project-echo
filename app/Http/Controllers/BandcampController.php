9ffa20b861cdeaa40fc12f802ac82514ca90ccd8
Mahdi Hazaveh <mahdi@hazaveh.net>
2025-06-29 18:58:32 +0200

    cache stuff for optimization

--- app/Http/Controllers/BandcampController.php
+++ app/Http/Controllers/BandcampController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Actions\Bandcamp\SearchAlbumAction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class BandcampController extends Controller
 {
     public function searchAlbums(string $search, SearchAlbumAction $action): array
     {
-        return $action->execute($search);
+        return Cache::remember("bandcamp-albums-" . md5($search), 60, function () use ($search, $action) {
+            return $action->execute($search);
+        });
     }
 }
