1cece5a83a07504c284214aa41b83e9ef75fc1e2
Mahdi Hazaveh <mahdi@hazaveh.net>
2025-07-01 18:26:35 +0200

    Echo log

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    \Illuminate\Support\Facades\Log::info('someone is visiting echo!');
     return redirect('https://postrocknation.com');
 });
 
