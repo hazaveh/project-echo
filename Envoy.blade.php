6fffadd6f0d7284fb7e47ffd24e26e372ae3be42
Mahdi Hazaveh <mahdi@hazaveh.net>
2025-12-29 17:57:54 +0100

    fix for permission

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,10 +1,10 @@
 @servers(['psilocin' => ['psilocin.empinet.com']])
 
 @task('deploy')
-    setfacl -R -m u:33:rwx /srv/stacks/project-echo
     cd /srv/stacks/project-echo
     git stash
     git pull origin master
+    setfacl -R -m u:33:rwx /srv/stacks/project-echo
     docker exec echo-api composer install
     docker exec echo-api php artisan migrate --force
     docker exec echo-api php artisan optimize
