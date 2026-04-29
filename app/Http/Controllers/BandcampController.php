67d3f017edb2f62d03b490b9ba87a40159e928f0
Mahdi Hazaveh <mahdi@hazaveh.net>
2026-04-29 09:52:01 +0200

    increased cache TTL

--- app/Http/Controllers/BandcampController.php
+++ app/Http/Controllers/BandcampController.php
@@ -13,14 +13,14 @@ class BandcampController extends Controller
 {
     public function searchAlbums(string $search, SearchAlbumAction $action): array
     {
-        return Cache::remember('bandcamp-albums-'.md5($search), 120, function () use ($search, $action) {
+        return Cache::remember('bandcamp-albums-'.md5($search), 360, function () use ($search, $action) {
             return $action->execute($search);
         });
     }
 
     public function artistAlbums(string $username, FetchArtistAlbumsAction $action): array
     {
-        return Cache::remember('bandcamp-artist-albums-'.md5($username), 120, function () use ($username, $action) {
+        return Cache::remember('bandcamp-artist-albums-'.md5($username), 360, function () use ($username, $action) {
             return $action->execute($username);
         });
     }
@@ -29,7 +29,7 @@ class BandcampController extends Controller
     {
         $cacheKey = 'bandcamp-album-details-v2-'.md5($username.'-'.$albumSlug);
 
-        return Cache::remember($cacheKey, 120, function () use ($username, $albumSlug, $action) {
+        return Cache::remember($cacheKey, 360, function () use ($username, $albumSlug, $action) {
             try {
                 return $action->execute($username, $albumSlug);
             } catch (RuntimeException $exception) {
@@ -43,7 +43,7 @@ class BandcampController extends Controller
     {
         $cacheKey = 'bandcamp-album-details-v2-'.md5($username.'-'.$releaseType.'-'.$albumSlug);
 
-        return Cache::remember($cacheKey, 120, function () use ($username, $releaseType, $albumSlug, $action) {
+        return Cache::remember($cacheKey, 360, function () use ($username, $releaseType, $albumSlug, $action) {
             try {
                 return $action->executeByType($username, $releaseType, $albumSlug);
             } catch (InvalidArgumentException|RuntimeException $exception) {
